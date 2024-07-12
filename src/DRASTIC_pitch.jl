656f34bb4e9fd741db07ec23f58004f777adc0e4
Luis MANZANILLAS VELEZ <lmanzanillas@gmail.com>
2024-07-12 17:39:24 +0200

    test

--- src/DRASTIC_pitch.jl
+++ src/DRASTIC_pitch.jl
@@ -69,5 +69,12 @@ function get_pitch(Data::Matrix{<:Real},calib=45.0,tolerance = 0.2)
 end
 
 function get_section_merged(base_dir::String,photo_list::Vector{Int64}, dx::Real,x_size::Real,y_size::Real,my_cte::Real=31.0,rot_angle::Real=0., expected_pitch::Real = 2.94)
-	println("Hello")
+    h5_files = base_dir*"/".*readdir(base_dir);
+    counter = 0
+    Column = []
+    d_cut_low = expected_pitch - 0.3
+    d_cut_high = expected_pitch + 0.3
+    x_cut_low = 150.
+    x_cut_high = x_size - 150.
+    println("Hello")
 end
